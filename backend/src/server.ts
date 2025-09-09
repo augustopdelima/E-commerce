96c9b45df300bd692bd4d3f7d42ab4328edae0e2
pachedres <lucaspacheco39@gmail.com>
2025-09-09 20:57:43 -0300

    Configuração inicial do projeto e backend com SQLite

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,17 @@
 import express from "express";
 const app = express();
+import { sequelize } from "./database";
+import { User } from "./models/User";
 
 const port = process.env.PORT ?? "3001";
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-  console.log("Response sent");
-});
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server rodando na porta: ${port}`);
+
+  // Sincroniza apenas o banco
+  sequelize.sync().then(() => {
+    console.log("Banco sincronizado!");
+  }).catch((err) => {
+    console.error("Erro ao sincronizar o banco:", err);
+  });
 });
