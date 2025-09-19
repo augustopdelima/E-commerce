d0079463d94bbe2298dcf0db19505eb2884da6eb
Augusto Pasquali <augusto.pasqualidelima@gmail.com>
2025-09-18 22:08:13 -0300

    add: login user

--- backend/src/routes/user_routes.ts
+++ backend/src/routes/user_routes.ts
@@ -1,14 +1,20 @@
 import { Router } from "express";
 import { UserController } from "../controllers/user_controller";
 import { UserService } from "../services/user_service";
+import { isAdmin } from "../middlewares/isAdmin";
 const router = Router();
 
-const SECRET = process.env.SECRET as string;
+
+const SECRET = process.env.SECRET ?? "supersecretaccesstoken";
+
+const REFRESH_SECRET = process.env.REFRESH_SECRET ??  "supersecretrefreshtoken";
 
 const userService = UserService();
-const userController = UserController(userService, SECRET);
+const userController = UserController(userService, SECRET,  REFRESH_SECRET);
 
 router.post("/register", userController.register);
-router.get("/:id", userController.user);
+router.get("/:id", isAdmin, userController.user);
+router.post("/login", userController.login);
+router.post("/refresh",  userController.refresh);
 
 export default router;
