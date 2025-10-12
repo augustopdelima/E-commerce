0d12efd6f3826d22c0a183d53b0ce753b3e5d40b
Augusto Pasquali <augusto.pasqualidelima@gmail.com>
2025-10-11 22:55:36 -0300

    Refact: adding error boundary to home

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -1,11 +1,22 @@
 import { Suspense } from "react";
 import { ProductsGrid } from "../../components/products-grid";
-
+import { ErrorBoundary } from "../../components/error-boundary";
+import { useQueryErrorResetBoundary } from "@tanstack/react-query";
+import { ProductErrorFallback } from "./product-error-fallback";
 
 export const Home = () => {
-    return(
-        <Suspense>
-            <ProductsGrid />
-        </Suspense>
-    )
+  const { reset } = useQueryErrorResetBoundary();
+
+  return (
+    <ErrorBoundary
+      fallback={({ resetErrorBoundary }) => (
+        <ProductErrorFallback resetErrorBoundary={resetErrorBoundary} />
+      )}
+      onReset={reset}
+    >
+      <Suspense fallback={<div>...Loading</div>}>
+        <ProductsGrid  />
+      </Suspense>
+    </ErrorBoundary>
+  );
 };
